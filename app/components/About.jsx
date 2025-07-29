4a00abdb22be73342d022009fb5e74abca0fe598
amit <amitmondoloishik@gmail.com>
2025-07-29 17:18:20 +0600

    about section added

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,8 +8,8 @@ const About = () => {
             <h4 className="text-center mb-2 text-lg">Introduction</h4>
             <h2 className="text-center text-5xl">About me</h2>
 
-            <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
-                <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+            <div className="flex w-full flex-col lg:flex-row items-center gap-20 md:my-20 my-7">
+                <div className="w-64 sm:w-80 rounded-3xl max-w-none hidden md:block">
                     <Image src={assets.user_image} alt="" className="w-full rounded-3xl" />
                 </div>
                 <div className="flex-1">
