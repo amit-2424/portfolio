b00ab7fb611de5f28e922cb44979f1d9d3f4b0a2
amit <amitmondoloishik@gmail.com>
2025-07-29 17:27:11 +0600

    abouot updated build

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const About = () => {
     return (
-        <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+        <div id="about" className="w-full px-[12%] md:py-10 scroll-mt-20">
             <h4 className="text-center mb-2 text-lg">Introduction</h4>
             <h2 className="text-center text-5xl">About me</h2>
 
